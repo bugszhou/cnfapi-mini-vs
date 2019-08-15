1e7237600667d8fd88c63d2df97938f5c9f464c5
bugszhou <bugszhou@outlook.com>
2019-08-15 14:43:34 +0800

    fix: 参数错误

--- src/lib/index.js
+++ src/lib/index.js
@@ -11,7 +11,7 @@ export default function(
     apiList,
     resInterceptor,
     openResInterceptor,
-    headers: {
+    headers = {
       'Content-Type': 'application/json',
     },
   } = { apiList: {} },
