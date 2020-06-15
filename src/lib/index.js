a27b15745679e36b36e4075cea0de2874e5f6320
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-06-15 11:54:34 +0800

    feat: Before 增加事件

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,84 +1,133 @@
-import Api from 'cnfapi-miniprogram';
-import sha256 from 'hash.js/lib/hash/sha/256';
+import Api from "cnfapi-miniprogram";
+import sha256 from "hash.js/lib/hash/sha/256";
 
 // eslint-disable-next-line
-export default function(
+export default function (
   {
-    baseURL = '',
-    env = 'weapp',
-    appKey = '',
-    appCode = '',
+    baseURL = "",
+    env = "weapp",
+    appKey = "",
+    appCode = "",
     apiList,
     resInterceptor,
     openResInterceptor,
     resSuccessCallback,
     headers = {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   } = { apiList: {} },
 ) {
-  const api = new Api({
-    baseURL,
-    env, // 使用环境：weapp - 微信小程序、aliapp - 支付宝小程序、swan - 百度小程序
-    timeout: 10000, // 10s超时
-    headers,
-    resSuccessCallback(data, next) {
-      if (resSuccessCallback && typeof resSuccessCallback === 'function') {
-        return resSuccessCallback.bind(this)(data, next, this);
-      }
-      if (data.code === 200) {
-        return next(null, data.data, data.code);
-      }
-      return next({
-        msg: data.msg,
-        retcode: data.code,
-      }, {}, data.code);
+  const api = new Api(
+    {
+      baseURL,
+      env, // 使用环境：weapp - 微信小程序、aliapp - 支付宝小程序、swan - 百度小程序
+      timeout: 10000, // 10s超时
+      headers,
+      resSuccessCallback(data, next) {
+        if (resSuccessCallback && typeof resSuccessCallback === "function") {
+          return resSuccessCallback.bind(this)(data, next, this);
+        }
+        if (data.code === 200) {
+          return next(null, data.data, data.code);
+        }
+        return next(
+          {
+            msg: data.msg,
+            retcode: data.code,
+          },
+          {},
+          data.code,
+        );
+      },
+      openResInterceptor(res) {
+        if (typeof openResInterceptor === "function") {
+          return openResInterceptor.call(this, {
+            ...res,
+            retcode: res ? res.code : "",
+          });
+        }
+        return false;
+      },
+      resInterceptor(serverData, next) {
+        if (typeof resInterceptor === "function") {
+          return resInterceptor.bind(this)(serverData, next, this);
+        }
+        return next();
+      },
     },
-    openResInterceptor(res) {
-      if (typeof openResInterceptor === 'function') {
-        return openResInterceptor.call(this, {
-          ...res,
-          retcode: res ? res.code : '',
-        });
-      }
-      return false;
-    },
-    resInterceptor(serverData, next) {
-      if (typeof resInterceptor === 'function') {
-        return resInterceptor.bind(this)(serverData, next, this);
-      }
-      return next();
-    },
-  }, apiList);
+    apiList,
+  );
 
   // eslint-disable-next-line
   api._before = function apiBefore(apiOpts, apiConf, next) {
     const { isSkipSign, params } = apiConf;
     let { data } = apiOpts;
+    let emit = (evt, evtData) => {
+      console.warn("this.emit is not a function: ", evt, " --- ", evtData);
+    };
+    if (typeof this.emit === "function") {
+      emit = this.emit.bind(this);
+    }
     if (!isSkipSign) {
+      emit("cnfapi:apiBefore:sign:before", {
+        apiOpts,
+        apiConf,
+      });
       let signData = {};
       if (data === null || data === undefined) {
         data = {};
         signData = data;
       }
+      emit("cnfapi:apiBefore:sign:signData", {
+        apiOpts,
+        apiConf,
+        signData,
+      });
       let postDataKeys = [];
       if (params && Array.isArray(params.post)) {
         postDataKeys = params.post;
       }
+      emit("cnfapi:apiBefore:sign:postDataKeys", {
+        apiOpts,
+        apiConf,
+        signData,
+        postDataKeys,
+      });
       postDataKeys.forEach((item) => {
         const val = data[item.param];
         if (val !== null && val !== undefined) {
           signData[item.param] = val;
         }
       });
+      emit("cnfapi:apiBefore:sign:signData:after", {
+        apiOpts,
+        apiConf,
+        signData,
+        postDataKeys,
+      });
       if (!apiConf.headers) {
         // eslint-disable-next-line
         apiConf.headers = {};
       }
       // eslint-disable-next-line
-      apiConf.headers['Authorization-AppKey'] = appKey;
+      apiConf.headers["Authorization-AppKey"] = appKey;
       // eslint-disable-next-line
-      apiConf.headers['Authorization-Sign'] = sha256().update(`${appKey}${JSON.stringify(signData)}${apiConf.headers['Authorization-Token'] || ''}${appCode}`).digest('hex');
+      apiConf.headers["Authorization-Sign"] = sha256()
+        .update(
+          `${appKey}${JSON.stringify(signData)}${
+            apiConf.headers["Authorization-Token"] || ""
+          }${appCode}`,
+        )
+        .digest("hex");
+      emit("cnfapi:apiBefore:sign:after", {
+        apiOpts,
+        apiConf,
+        signData,
+        sign: apiConf.headers["Authorization-Sign"],
+        signStr: `${appKey}${JSON.stringify(signData)}${
+          apiConf.headers["Authorization-Token"] || ""
+        }${appCode}`,
+      });
     }
     next({
       ...apiOpts,
